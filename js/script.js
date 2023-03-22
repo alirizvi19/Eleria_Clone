bac9aee73d316d70975a52609b877cb98a0cf604
Ali Rizvi <ali.ammar.rizvi13@gmail.com>
2023-03-22 11:35:08 +0530

    update-22.03.2023

--- js/script.js
+++ js/script.js
@@ -23,21 +23,23 @@ $(document).ready(function () {
     return false;
   });
 
-  // Add parallax scrolling to all images in .paralax-image container
-  $(".parallax-image").each(function () {
-    // only put top value if the window scroll has gone beyond the top of the image
-    if ($(this).offset().top < $(window).scrollTop()) {
-      // Get ammount of pixels the image is above the top of the window
-      var difference = $(window).scrollTop() - $(this).offset().top;
-      // Top value of image is set to half the amount scrolled
-      // (this gives the illusion of the image scrolling slower than the rest of the page)
-      var half = difference / 10 + "px";
+  $(window).scroll(function () {
+    // Add parallax scrolling to all images in .paralax-image container
+    $(".parallax-image").each(function () {
+      // only put top value if the window scroll has gone beyond the top of the image
+      if ($(this).offset().top < $(window).scrollTop()) {
+        // Get ammount of pixels the image is above the top of the window
+        var difference = $(window).scrollTop() - $(this).offset().top;
+        // Top value of image is set to half the amount scrolled
+        // (this gives the illusion of the image scrolling slower than the rest of the page)
+        var half = difference / 4 + "px";
 
-      $(this).find("img").css("top", half);
-    } else {
-      // if image is below the top of the window set top to 0
-      $(this).find("img").css("top", "0");
-    }
+        $(this).find("img").css("top", half);
+      } else {
+        // if image is below the top of the window set top to 0
+        $(this).find("img").css("top", "0");
+      }
+    });
   });
 });
 
